e9f520eaae5d730aabf05736ce1243e79bdd1faf
ualia946 <i946@inlumine.ual.es>
2026-05-01 10:50:41 +0200

    chore: configurar grupo de recursos y backend remoto de terraform

---
+++ azure-infraestructure/main.tf
@@ -0,0 +1,26 @@
+terraform {
+  required_providers {
+    azurerm = {
+        source = "hashicorp/azurerm"
+        version = "~> 3.0.2"
+    }
+  }
+  backend "azurerm" {
+    resource_group_name = "rg-terraform-state"
+    storage_account_name = "stiotsleepive"
+    container_name = "tfstate"
+    key = "iot-sleep.terraform.tfstate"
+  }
+  required_version = ">= 1.1.0"
+}
+
+provider "azurerm" {
+  features {
+    
+  }
+}
+
+resource "azurerm_resource_group" "rg" {
+  name = "rg-iot-sleep"
+  location = "spaincentral"
+}
